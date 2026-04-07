c731a16b53fbcc72f37630183b624371c50328c3
ahkishakiunisala <ahkishamikellekiunisalanika>
2026-04-08 06:46:01 +0800

    fixed the links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>HOME</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
